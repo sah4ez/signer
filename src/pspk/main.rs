4c2f640a0e88f5b66d99435187cdd2527a348f76
Alexandr Kozlenkov <sah4ez32@gmail.com>
2019-01-30 01:23:42 +0300

    Successful save and load PK from mongo

--- src/pspk/main.rs
+++ src/pspk/main.rs
@@ -1,41 +1,71 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+#![feature(proc_macro_hygiene, decl_macro, try_from)]
 
 #[macro_use] extern crate rocket;
 extern crate rocket_contrib;
 #[macro_use] extern crate serde_derive;
 extern crate serde;
+extern crate mongodb;
+extern crate hex;
 
-use std::collections::HashMap;
-use std::sync::RwLock;
+
 use rocket::State;
+use base64::{decode, encode};
+use hex::{decode as hdecode};
 use rocket_contrib::json::Json;
-
-struct PublicKeys  {
-    store:  HashMap<String,String>,
-}
+use mongodb::{Bson, bson, doc};
+use mongodb::{Client, ThreadedClient};
+use mongodb::db::ThreadedDatabase;
+use mongodb::coll::Collection;
+use mongodb::spec::BinarySubtype;
+use std::prelude::v1::Vec;
 
 #[derive(Serialize, Deserialize)]
 struct Key {
     key: String,
 }
 
-#[post("/<name>", format = "application/json", data = "<key>")]
-fn add(name: String, key: Json<Key>, current_store: State<RwLock<PublicKeys>>) {
-    let mut cur = current_store.write().expect("map lock");
-    cur.store.insert(name, key.0.key);
+#[post("/<key>", format = "application/json", data = "<value>")]
+fn add(key: String, value: Json<Key>, current_store: State<Collection>) {
+    let b = match decode(&value.0.key) {
+        Ok(bb) => bb.to_vec(),
+        _ => [0; 32].to_vec(),
+    };
+
+    if b != [0;32].to_vec() && b.len() == 32 {
+        let k = Bson::Binary(BinarySubtype::Generic, Vec::from(b.as_slice()));
+        current_store.insert_one(doc!{"key":key, "value": k }, None).unwrap();
+    }
 }
 
-#[get("/<name>")]
-fn get(name: String, current_store: State<RwLock<PublicKeys>>) -> String {
-    let key = current_store.read().unwrap();
-    let key = key.store.get(name.as_str()).unwrap();
+#[get("/<key>")]
+fn get(key: String, current_store: State<Collection>) -> String {
+    let value = match current_store.find_one(Some(doc!{"key": key}), None).
+        expect("not found") {
+        Some(value) => value,
+            _ => doc!{},
+    };
 
-    format!("{}", &key)
+
+    match value.get("value") {
+        Some(f) => {
+            encode( hdecode(
+                    String::from(f.to_string())
+                        .as_str()[13..77]
+                        .as_bytes()
+            ).unwrap().as_slice())
+        },
+        None => String::from("not found"),
+    }.parse().unwrap()
 }
 
 fn main() {
+    let client = Client::with_uri("mongodb://localhost:27017")
+        .expect("Failed to initialize standalone client.");
+
+    let coll = client.db("pspk)").collection("keys");
+
     rocket::ignite()
-        .manage(RwLock::new(PublicKeys{ store: HashMap::<String, String>::new() }))
+        .manage(coll)
         .mount("/", routes![get, add]).launch();
 }
 
